449f5e069d3364be55b2c76ee6c1d2b6218a0aa3
jaehyoungpark <jaehyoungpark@oden.kr>
2023-05-16 21:53:26 +0900

    flutter practice 2

---
+++ toonflixpractice2/android/app/src/main/kotlin/com/example/toonflixpractice2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.toonflixpractice2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ toonflixpractice2/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  void onClicked() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const MyLargeTitle(),
+              Transform.rotate(
+                angle: 150,
+                child: IconButton(
+                    iconSize: 40,
+                    onPressed: onClicked,
+                    icon: const Icon(
+                      Icons.add,
+                      color: Colors.black,
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context)
+            .textTheme
+            .titleLarge
+            ?.color, // context를 이용해 부모요소에 접근 가능
+      ),
+    );
+  }
+}
